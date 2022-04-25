965197671e12042dcc2a4a8c41616a63cf2e10bd
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-25 12:45:33 +0530

    Fetched

---
+++ UC2_DiceNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderSimulator
+{
+    class UC2_DiceNumer
+    {
+        public class SnakeAndLadder
+        {
+            private int position;
+            private bool winCheck;
+            private int totalDiceRoll;
+
+            static Random random = new Random();
+
+            public SnakeAndLadder()
+            {
+                position = 0;
+                winCheck = false;
+                totalDiceRoll = 0;
+            }
+
+            public void Describe()
+            {
+                Console.WriteLine("Player at " + position + " position.");
+            }
+
+            public void RollDice()
+            {
+                totalDiceRoll++;
+                
+                int Roll = random.Next(1, 7);
+                Console.WriteLine("DIce Number: " + Roll);
+                Console.WriteLine("Total dice rolled" + totalDiceRoll);
+            }
+
+        }
+    }
+}
