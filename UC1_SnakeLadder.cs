5990b47d5d906a6acbd1a1d2b887849d55b38a63
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-25 12:43:00 +0530

    Fetched

---
+++ UC1_SnakeLadder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadderSimulator
+{
+
+    public class UC1_SnakeLadder
+    {
+        public class SnakeAndLadder
+        {
+            private int position;
+            private bool winCheck;
+            private int totalDiceRoll;
+
+            static Random random = new Random();
+
+            public SnakeAndLadder()
+            {
+                position = 0;
+                winCheck = false;
+                totalDiceRoll = 0;
+            }
+
+            public void Describe()
+            {
+                Console.WriteLine("Player at " + position + " position.");
+            }
+
+        }
+    }
+    
+}
