2b4d3f0b2df1c0d9d5f113b082018d54ea3ca492
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-25 13:00:27 +0530

    Fetched

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SnakeAndLadderSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder game");
+            Console.WriteLine("Snake and Ladder!");
+            SnakeAndLadder player1 = new SnakeAndLadder();
+            SnakeAndLadder player2 = new SnakeAndLadder();
+            Console.WriteLine("Player 1: ");
+            player1.Describe();
+            Console.WriteLine("Player 2: ");
+            player2.Describe();
+            player1.PlayTillEnd(player2);
+        }
+    }
+}
